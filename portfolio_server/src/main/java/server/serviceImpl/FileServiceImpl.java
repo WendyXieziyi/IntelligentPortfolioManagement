580170fa5aeda1f052850d06e27e7e619e36f0bf
Xieziyi <171840012@nju.edu.cn>
2019-05-27 20:23:22 +0800

    update server

--- portfolio_server/src/main/java/server/serviceImpl/FileServiceImpl.java
+++
@@ -1,8 +0,0 @@
-package server.serviceImpl;
-
-import org.springframework.stereotype.Service;
-import server.service.FileService;
-
-@Service(value = "fileService")
-public class FileServiceImpl implements FileService {
-}
